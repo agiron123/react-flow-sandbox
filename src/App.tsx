6b95125a96aee9343a22ce53478af3ec7892622e
Andre Giron <andre@spiralai.co>
2023-02-03 12:12:53 -0800

    Inital project setup.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React Flow Sandbox</h2>
+      <p>elkjs and react-flow have been added to package.json</p>
+      <p>To get started run:</p>
+      <code>npm install</code>
+      <br />
+      <code>npm run dev</code>
+    </div>
+  )
+}
+
+export default App
